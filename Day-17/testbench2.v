aa1efafeb3ff28a2bf5a8ef94a1d2ff6fb6e7f24
Murugesh06 <murugeshrajrp@gmail.com>
2025-09-21 16:28:45 +0530

    Create testbench2.v

---
+++ Day-17/testbench2.v
@@ -0,0 +1,17 @@
+module top_module();
+reg clk,in;
+    reg [2:0]s;
+    wire out;
+    q7 uut(clk,in,s,out);
+    initial begin
+        clk=0;in=0;s=2;
+        #10 s=6;
+        #10 in=1;s=2;
+        #10 in=0;s=7;
+        #10 in=1;s=0;
+        #30 in=0;
+    end
+    always begin
+        #5 clk=~clk;
+    end
+endmodule
